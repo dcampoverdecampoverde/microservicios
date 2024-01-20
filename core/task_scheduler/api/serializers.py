394df2b644f0b16a7f4ec1c9315e4310bb931f21
Danny Campoverde <dcampoverdecampoverde@gmail.com>
2024-01-20 15:37:51 -0500

    nuevos arhivos

---
+++ core/task_scheduler/api/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+
+from task_scheduler.models import *
+
+
+class TareaJobRegistroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = programador_jobs
+        fields = [
+            'estado',
+            'nombre_tarea',
+            'dias_semana',
+            'tipo_horario',
+            'horario_rango',
+            'job_descripcion',
+            'job_ejecutar',
+            'emails_notificacion',
+            'usuario_registro',
+            'terminal_registro'
+        ]
+
+
+class TareaJobListaSerializer(serializers.ModelSerializer):
+    fecha_registro = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+    fecha_modificacion = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+    fecha_ultima_ejecucion = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+    dias_semana_numeros = serializers.SerializerMethodField()
+
+    class Meta:
+        model = programador_jobs
+        fields = [
+            'id',
+            'estado',
+            'estado_ejecucion',
+            'nombre_tarea',
+            'dias_semana',
+            'dias_semana_numeros',
+            'tipo_horario',
+            'horario_rango',
+            'job_descripcion',
+            'job_ejecutar',
+            'emails_notificacion',
+            'fecha_ultima_ejecucion',
+            'observaciones',
+            'usuario_registro',
+            'terminal_registro',
+            'fecha_registro',
+            'usuario_modificacion',
+            'terminal_modificacion',
+            'fecha_modificacion'
+        ]
+
+    def get_dias_semana_numeros(self, obj):
+        return obj
+
+
+class TareaJobActualizarEjecucionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = programador_jobs
+        fields = [
+            'estado_ejecucion',
+            'usuario_modificacion',
+            'terminal_modificacion',
+            'fecha_modificacion'
+            'fecha_ultima_ejecucion'
+        ]
---
+++ core/task_scheduler/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class programador_jobs(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    estado = models.CharField(max_length=1, null=True)
+    estado_ejecucion = models.CharField(max_length=50, null=True)
+    nombre_tarea = models.CharField(max_length=100)
+    dias_semana = models.CharField(max_length=300, null=True)
+    tipo_horario = models.CharField(max_length=50, null=True)
+    horario_rango = models.CharField(max_length=50, null=True)
+    job_descripcion = models.CharField(max_length=100, null=True)
+    job_ejecutar = models.CharField(max_length=200, null=True)
+    emails_notificacion = models.CharField(max_length=500, null=True)
+    fecha_ultima_ejecucion = models.DateTimeField(null=True)
+    observaciones = models.CharField(max_length=500, null=True, blank=True)
+    usuario_registro = models.CharField(max_length=20, null=True)
+    terminal_registro = models.CharField(max_length=15, null=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True, null=True)
+    usuario_modificacion = models.CharField(max_length=20, null=True)
+    terminal_modificacion = models.CharField(max_length=15, null=True)
+    fecha_modificacion = models.DateTimeField(null=True)
